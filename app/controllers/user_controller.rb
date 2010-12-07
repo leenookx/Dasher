2c8d4e4c354639b671189fdd9b8b91385f3deb37
Lee Nookx <lnookx@googlemail.com>
2010-12-07 23:04:19 +0000

    Basic login system implemented. No way this should ever be used in reality but then this isn't reality is it...

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,14 +1,25 @@
 class UserController < ApplicationController
   def login
+    @user = User.new
+    @user.name = params[:name]
   end
 
   def process_login
+    if user = User.authenticate(params[:user])
+      session[:id] = user.id # Remember the user's id during this session
+      redirect_to session[:return_to] || '/'
+    else
+      flash[:error] = 'Invalid login.'
+      redirect_to :action => 'login', :name => params[:user][:name]
+    end
   end
 
   def logout
+    reset_session
+    flash[:message] = 'Logged out.'
+    redirect_to :action => 'login'
   end
 
   def my_account
   end
-
-end
+end 

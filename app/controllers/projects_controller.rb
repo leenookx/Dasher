2d0eb102c6770c3d5ad73be75e4e71fdd49a1052
lnookx <lnookx@googlemail.com>
2011-02-12 17:51:25 +0000

    Allow a user (with the proper authentication) the ability to get a list of all of the projects that they are part of.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,17 +1,51 @@
 class ProjectsController < ApplicationController
   layout 'standard'
-  before_filter :login_required
+#  before_filter :login_required, :only => [:index, :new, :create]
 
+
+  # #####################################################
+  #
+  # #####################################################
   def index
-    @title = "Dasher - Your Projects"
+    user = validate_user( request.headers["authentication-token"] )
+    if user.nil?
+      respond_to do |format|
+        format.html do
+          flash[:error] = 'Not authenticated.'
+          redirect_to root_url
+        end
 
-    @projects = Project.find_all_by_owner(@user.id)
+        format.xml { render :xml => { :status => :error, :message => 'Invalid authentication code.'}.to_xml, :status => 403 }
+
+        format.json { render :json => { :status => :error, :message => 'Invalid authentication code.'}.to_json, :status => 403 }
+      end
+    else
+      projects = Project.find_all_by_owner( user.id )
+      if projects
+        respond_to do |format|
+          format.html {     
+            @title = "Dasher - Your Projects"
+            @projects = Project.find_all_by_owner(@user.id)
+          }
+          format.xml  { render :xml => projects.to_xml( :only => [:id, :name] ) }
+          format.json { render :json => projects.to_json( :only => [:id, :name] ) }
+        end
+      end
+    end
   end
 
+
+  # #####################################################
+  #
+  # #####################################################
   def new
     @title = "Dasher - Create a new project"
   end
 
+
+  # #####################################################
+  #
+  # #####################################################
   def create
     if request.post? and params[:project]
       @project = Project.new(params[:project])
@@ -24,5 +58,50 @@ class ProjectsController < ApplicationController
     end
 
     redirect_to :action => 'index'
+  end
+
+  # #####################################################
+  # GET /projects/1
+  # GET /projects/1.xml
+  # GET /projects/1.json
+  # #####################################################
+  def show
+    user = validate_user( request.headers["authentication-token"] )
+    if user.nil?
+      respond_to do |format|
+        format.html do
+          flash[:error] = 'Not authenticated.'
+          redirect_to root_url
+        end
+
+        format.xml { render :xml => { :status => :error, :message => 'Invalid authentication code.'}.to_xml, :status => 403 }
+
+        format.json { render :json => { :status => :error, :message => 'Invalid authentication code.'}.to_json, :status => 403 }
+      end
+    else
+      projects = Project.find_all_by_owner( user.id )
+      if projects
+        respond_to do |format|
+          format.html { render :action => "new" }
+          format.xml  { render :xml => link.errors, :status => :unprocessable_entity }
+          format.json { render :json => link.errors, :status => :unprocessable_entity }
+        end
+      end
+    end
+  end
+
+private
+
+  # #####################################################
+  # 
+  # #####################################################
+  def validate_user(params)
+    return @user unless @user.nil?
+
+    if params && !params.empty?
+      return User.find_by_authentication_code( params )
+    else
+      return nil
+    end
   end
 end
